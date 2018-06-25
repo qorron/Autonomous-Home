3f64c504e02f6bcd2c225ccd20a7b3e3743f677b
quattro <808330+qorron@users.noreply.github.com>
2018-06-26 00:12:20 +0200

    add deploy script.
    the reson for this is that I want to be able to develop stuff independently in my workspace so the 'stable' versions can be called by cronjobs all the time and no half baked code is run on 'production'.
    some day this will be replaced by jenkins or something similar

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INSTALL_PATH='/usr/local/lib/home_automation/'
+CONFIG_PATH='/etc/home_automation/'
+
+# copy stuff from workspace to the final locations
+# some parts are not yet ready to be released and thus commented out
+
+# cp inverter.pl ${INSTALL_PATH}
+# cp heat_collector.pl ${INSTALL_PATH}
+# cp heat_.pl ${INSTALL_PATH}heat_
+# cp mqtt_.pl ${INSTALL_PATH}
+# cp AP_names.pl ${INSTALL_PATH}
+# cp get_pressure.pl ${INSTALL_PATH}
+# cp shutter.pl ${INSTALL_PATH}
+# cp strom.pl ${INSTALL_PATH}
+# cp solar.pl ${INSTALL_PATH}
+# cp prometheus.psgi ${INSTALL_PATH}
+# cp auto_ac.pl ${INSTALL_PATH}
+cp get_weather.pl ${INSTALL_PATH}
+
+
+# cp qmel.pm ${INSTALL_PATH}perl/
+cp get_weather.pm ${INSTALL_PATH}perl/
+cp config.pm ${INSTALL_PATH}perl/
+
+cp config/* ${CONFIG_PATH}
