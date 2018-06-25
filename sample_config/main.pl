51f8e9971cfe859b0ca61a1f12c18d2d4a1567c9
quattro <808330+qorron@users.noreply.github.com>
2018-06-26 00:03:39 +0200

    add a sample config

---
+++ sample_config/main.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.020;
+use Data::Dumper;
+
+%config = (
+	latitude           => 51.1789,
+	longitude          => -1.8262,
+	weather_cache_file => '/tmp/weather.storable',
+);
+
