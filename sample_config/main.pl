bf65178482d8b6cbb68861550633dc08b8bb960a
quattro <808330+qorron@users.noreply.github.com>
2018-07-10 11:19:36 +0200

    update sample config to include sensors_collector config

--- sample_config/main.pl
+++ sample_config/main.pl
@@ -10,3 +10,34 @@ use Data::Dumper;
 	weather_cache_file => '/tmp/weather.storable',
 );
 
+$config{host} = {
+	grid   => '10.0.0.2',    # device measuring el. power flow to and from the grid
+	solar  => '10.0.0.3',    # inverter, tells the power currently produces by the panels
+	symcon => '10.0.0.4',    # local symcon device controlling the heater
+};
+
+$config{files} = {
+	power_balance_file   => '/tmp/power_balance.db',
+	weather_cache_file   => '/tmp/weather.storable',
+	heater_json_file     => "/tmp/heat.json",
+	solar_json_file      => "/tmp/solar.json",
+	power_flow_json_file => "/tmp/power_flow.json",
+};
+
+$config{heater} = {
+	symcon_auth => 'Basic base64ed_password',    # get that by observing the web UI
+	symcon_root => 123,                          # root id, use web UI & inspect
+};
+
+# when rooms are differently named in the symcon unite
+$config{heater}{translation} = {
+	'Laboratory' => 'Lab',
+	'Library'    => "B\x{fc}ro",
+};
+
+# power rating for the heater units in every room
+$config{heater}{power} = {
+	'Library'    => 732,
+	'Laboratory' => 1423,
+};
+
